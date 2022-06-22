b6e1e58df5739e725078a1944eaf02f967a90f75
liushuai <794531178@qq.com>
2022-06-22 11:03:20 +0800

    基础练习git

--- leetcode_1/leetcode_1/src/com/ls/leetcodedemo.java
+++ leetcode_1/leetcode_1/src/com/ls/leetcodedemo.java
@@ -2,6 +2,6 @@ package com.ls;
 
 public class leetcodedemo {
     public static void main(String[] args) {
-        System.out.println("git 测试1");
+        System.out.println("git 新建分支测试");
     }
 }
